88fdc3d20c7b9f2dfdb92052ad69ee1ee9b384a5
Tom <997707754@qq.com>
2023-04-07 13:07:29 +0800

    add: lvim plugin hop

--- .config/lvim/lua/extra-plugin.lua
+++ .config/lvim/lua/extra-plugin.lua
@@ -9,4 +9,13 @@ lvim.plugins = {
       vim.g.mkdp_auto_start = 1
     end,
   },
+
+  -- Hop (Like Easymotion)
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+    end,
+  },
 }
