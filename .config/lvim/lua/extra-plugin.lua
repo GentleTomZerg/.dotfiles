06861b9def4b54dda16793f8f2b288535fb1df12
Tom <997707754@qq.com>
2023-05-15 00:01:21 +0800

    update: livm rofi

--- .config/lvim/lua/extra-plugin.lua
+++ .config/lvim/lua/extra-plugin.lua
@@ -11,11 +11,21 @@ lvim.plugins = {
   },
 
   -- Hop (Like Easymotion)
+  -- {
+  --   "phaazon/hop.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("hop").setup()
+  --   end,
+  -- },
+
   {
     "phaazon/hop.nvim",
     event = "BufRead",
     config = function()
       require("hop").setup()
+      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
     end,
   },
 }
