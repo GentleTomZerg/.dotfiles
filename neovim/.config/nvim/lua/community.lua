993890dcd6d1d3828a6638ed872645c7e6979d8b
tom <void@void.com>
2024-05-23 14:26:37 +0800

    [Update] community plugin to astronvim

--- neovim/.config/nvim/lua/community.lua
+++ neovim/.config/nvim/lua/community.lua
@@ -4,31 +4,34 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  -- colorscheme
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  {
-    "catppuccin",
-    opts = {
-      flavour = "mocha",
-      transparent_background = true,
-    },
-  },
+	"AstroNvim/astrocommunity",
+	-- colorscheme
+	{ import = "astrocommunity.colorscheme.catppuccin" },
+	{
+		"catppuccin",
+		opts = {
+			flavour = "mocha",
+			transparent_background = true,
+		},
+	},
 
-  -- Language Packs
-  { import = "astrocommunity.pack.java" },
-  {
-    "nvim-jdtls",
-    keys = {
-      { "<leader>df", "<cmd> lua require'jdtls'.test_class() <cr>", desc = "Test all" },
-      { "<leader>dn", "<cmd> lua require'jdtls'.test_nearest_method() <cr>", desc = "Test method" },
-    },
-  },
+	-- Language Packs
+	{ import = "astrocommunity.pack.java" },
+	{
+		"nvim-jdtls",
+		keys = {
+			{ "<leader>df", "<cmd> lua require'jdtls'.test_class() <cr>", desc = "Test all" },
+			{ "<leader>dn", "<cmd> lua require'jdtls'.test_nearest_method() <cr>", desc = "Test method" },
+		},
+	},
 
-  { import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.lua" },
 
-  { import = "astrocommunity.pack.markdown" },
+	{ import = "astrocommunity.pack.markdown" },
 
-  { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.pack.html-css" },
+	{ import = "astrocommunity.pack.cpp" },
+	{ import = "astrocommunity.pack.html-css" },
+
+	-- markdown preview
+	{ import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
 }
