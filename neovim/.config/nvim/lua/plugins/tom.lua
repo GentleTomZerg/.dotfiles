c8eebb1d6a849e024ca67ee415d27638331c4f0a
tom <void@void.com>
2025-01-21 15:52:50 +0800

    [Update] make neo tree source selector bar transparent!

--- neovim/.config/nvim/lua/plugins/tom.lua
+++ neovim/.config/nvim/lua/plugins/tom.lua
@@ -1,6 +1,6 @@
 -- My customize config
--- For Community Plugin
 return {
+	-- For Community Plugin
 	{
 		"catppuccin",
 		opts = {
@@ -13,6 +13,25 @@ return {
 		keys = {
 			{ "<leader>df", "<cmd> lua require'jdtls'.test_class() <cr>",          desc = "Test all" },
 			{ "<leader>dn", "<cmd> lua require'jdtls'.test_nearest_method() <cr>", desc = "Test method" },
+		},
+	},
+	-- For Astro Built-in Plugins
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		opts = {
+			-- neo-tree separator transparent
+			source_selector = {
+				separator = { left = "", right = "" },
+				highlight_tab = "NeoTreeTabInactive",
+				highlight_tab_active = "NeoTreeTabActive",
+				highlight_background = "NeoTreeTabActive",
+				highlight_separator = "NeoTreeTabSeparatorActive",
+				highlight_separator_active = "NeoTreeTabSeparatorActive",
+			},
+			-- neo-tree window
+			window = {
+				position = "float",
+			},
 		},
 	},
 }
