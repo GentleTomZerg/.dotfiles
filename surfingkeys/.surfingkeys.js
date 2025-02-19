ce41303627f57d1a5e6f85fd5c2a2d773481ed11
tom <void@void.com>
2025-02-19 14:40:54 +0800

    [Update] surfingkeys keybindings

--- surfingkeys/.surfingkeys.js
+++ surfingkeys/.surfingkeys.js
@@ -24,10 +24,12 @@ const {
     vunmap
 } = api;
 
+// Map H and L to switch between tabs
 api.unmap('H');
 api.map('H', 'E');
 api.unmap('L');
 api.map('L', 'R');
+// Map J and K to move forward or backwards within a tab
 api.unmap('J');
 api.map('J', 'S');
 api.unmap('K');
