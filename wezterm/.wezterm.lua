8ff8547659a458b66c10cd8a9dccb9b5551be9fc
tom <tom@mac.com>
2025-06-17 23:10:15 +0800

    [Add] change default term to wezterm for mac

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,16 +4,24 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- Fonts
 config.font_size = 14
+config.font = wezterm.font("FiraCode Nerd Font")
 
+-- Window
 config.enable_tab_bar = false
-
 config.window_decorations = "RESIZE"
-
 config.window_background_opacity = 0.7
 config.macos_window_background_blur = 20
 
+-- Color scheme
 config.color_scheme = "One Dark (Gogh)"
+
+-- Disable Ligatures
+config.harfbuzz_features = { "calt = 0", "clig = 0", "liga = 0" }
+
+-- Cursor
+config.cursor_thickness = 4
 
 -- Finally, return the configuration to wezterm:
 return config
