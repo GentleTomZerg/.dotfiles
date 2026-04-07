2501998183adc76bec543e2b7ae21254aa9e8d3d
tom <tom@mac.com>
2026-04-07 21:37:06 +0800

    [chore] change neovim makrdown preview plugin to peek.nvim

--- neovim/.config/nvim/lua/community.lua
+++ neovim/.config/nvim/lua/community.lua
@@ -21,7 +21,7 @@ return {
   { import = "astrocommunity.pack.python.basedpyright" },
   { import = "astrocommunity.pack.python.ruff" },
   -- markdown preview
-  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.markdown-and-latex.peek-nvim" },
   { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   -- color display
   { import = "astrocommunity.color.nvim-highlight-colors" },
