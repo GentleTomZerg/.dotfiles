10a532f3b2ea306a43d3aba4563a3a5f9a87c8aa
tom <void@void.com>
2026-05-06 15:57:37 +0800

    [feat] obsidian.nvim for note taking, and tmux session config for obsidian

---
+++ tmuxifier/layouts/obsidian.session.sh
@@ -0,0 +1,15 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "$HOME/Documents/Obsidian Vault"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "obsidian"; then
+  window_root "$HOME/Documents/Obsidian Vault"
+  new_window "note"
+
+  select_window "note"
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
