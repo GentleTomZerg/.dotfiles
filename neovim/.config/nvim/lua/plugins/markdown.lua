f68b74b6354635ba2858ab8707daff37fa20352b
tom <tom@mac.com>
2026-05-08 23:57:24 +0800

    [feature] add nvim plugin support for image and mermaid preview

--- neovim/.config/nvim/lua/plugins/markdown.lua
+++ neovim/.config/nvim/lua/plugins/markdown.lua
@@ -30,13 +30,25 @@ return {
       },
     },
   },
-
-  -- install with yarn or npm
   {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
-    ft = { "markdown" },
+    -- prerequisite: mermaid-cli
+    "3rd/image.nvim",
+    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    opts = {
+      processor = "magick_cli",
+      integrations = {
+        markdown = {
+          only_render_image_at_cursor = true,
+        },
+      },
+    },
+  },
+  {
+    -- prerequisite: npx puppeteer browsers install chrome-headless-shell
+    "3rd/diagram.nvim",
+    dependencies = {
+      { "3rd/image.nvim", opts = {} }, -- you'd probably want to configure image.nvim manually instead of doing this
+    },
+    opts = {},
   },
 }
