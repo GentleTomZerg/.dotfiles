be535df51c278045eb99612806028f795b53f193
tom <void@void.com>
2026-05-09 10:59:03 +0800

    [feature] use snacks.vim image plugin to render img latex and mermaid in md

--- neovim/.config/nvim/lua/plugins/markdown.lua
+++ neovim/.config/nvim/lua/plugins/markdown.lua
@@ -1,3 +1,7 @@
+-- Prerequisite:
+-- mermaid-cli: mermaid graph preview
+-- imageMagick: image preview
+-- tectonic: latex preview
 return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
@@ -20,35 +24,23 @@ return {
     },
     opts = {
       latex = {
-        enabled = true,
-        render_modes = false,
-        converter = { "utftex", "latex2text" },
-        highlight = "RenderMarkdownMath",
-        position = "center",
-        top_pad = 0,
-        bottom_pad = 0,
+        enabled = false,
       },
     },
   },
   {
-    -- prerequisite: mermaid-cli
-    "3rd/image.nvim",
-    build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+    "folke/snacks.nvim",
     opts = {
-      processor = "magick_cli",
-      integrations = {
-        markdown = {
-          only_render_image_at_cursor = true,
+      image = {
+        doc = {
+          enabled = true,
+          inline = true,
+          float = true,
+        },
+        math = {
+          enabled = true,
         },
       },
     },
-  },
-  {
-    -- prerequisite: npx puppeteer browsers install chrome-headless-shell
-    "3rd/diagram.nvim",
-    dependencies = {
-      { "3rd/image.nvim", opts = {} }, -- you'd probably want to configure image.nvim manually instead of doing this
-    },
-    opts = {},
   },
 }
